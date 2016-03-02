7325afdd85d1747e75efb30ab43287b5ec859084
patrick <patrick@knowhere.guru>
2016-03-02 11:54:16 +0100

    Add Angular 1.5

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,13 @@ $( document ).ready(function() {
     $(document).foundation();
     console.log( "knowhere template ready!" );
 });
+
+var app = require('angular').module('angularApp', []).config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}'); 
+});
+
+app.controller('TestAngularCtrl', function($scope) {
+  $scope.angularVar = "Hello Angular Scope";
+  
+  console.log("Hello Angular");
+});
